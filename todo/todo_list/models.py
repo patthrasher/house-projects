3a3852bc50a08554ce7ef296d00fef253562b410
patthrasher <patrickalanthrasher@gmail.com>
2020-10-21 11:11:40 -0500

    edited models and base.html

--- todo/todo_list/models.py
+++ todo/todo_list/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class List(models.Model) :
     item = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    room = models.CharField(max_length=200)
 
     def __str__(self) :
         return self.item + ' | ' + str(self.completed)
